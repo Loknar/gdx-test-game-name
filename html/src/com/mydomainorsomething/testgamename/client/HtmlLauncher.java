796335483ec8ffd358539e1b9b38d1c87a3de04a
Loknar <svefgud@gmail.com>
2016-12-01 23:55:33 +0000

    Initial commit

---
+++ html/src/com/mydomainorsomething/testgamename/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mydomainorsomething.testgamename.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mydomainorsomething.testgamename.MyGdxTestGameName;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxTestGameName();
+        }
+}
